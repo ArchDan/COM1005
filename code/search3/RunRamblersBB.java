55fd535b07809b794751976acbde50bfbe9012c6
ArchDan <44615234+ArchDan@users.noreply.github.com>
2021-05-19 15:56:30 +0100

    Created necessary classes to begin
    Created Ramblers Classes for Branch and Bound as well as the RunRamblers class for BB.

---
+++ code/search3/RunRamblersBB.java
@@ -0,0 +1,7 @@
+public class RunRamblersBB {
+    public static void main ( String [] args ) {
+        
+        // a main to run and test Ramblers Branch and Bound from
+        System.out.println("Running RamblersBB.java");
+    }
+}
